c0a81b19010f1152c956e4061a78abb3348fe5c3
Lucky Bisht <lcky7410@gmail.com>
2026-06-06 17:57:28 +0000

    demo

--- diameterOfBinary.cpp
+++ diameterOfBinary.cpp
@@ -3,7 +3,7 @@ class Solution{
     int height(TreeNode* root , int &maxi){
         if(root==NULL) return 0 ;
         int l= height(root->left,maxi);
-        int r= height(root->right,maxi);
+        int r= height(root ->right,maxi);
         maxi= max(maxi ,l+r);
         return max(l,r)+1;
     }
