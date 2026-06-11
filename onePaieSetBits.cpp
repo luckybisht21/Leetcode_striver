68ab6826141ad6f755dc109ffb1a5a2bcded953f
Lucky Bisht <lcky7410@gmail.com>
2026-06-11 20:34:56 +0000

    set bits exact one pair

---
+++ onePaieSetBits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool consecutiveSetBits(int n) {
+        bitset<20>b(n);
+        int count=0;
+        for(int i=0;i<b.size()-1;i++){
+            if(b[i]==1 && b[i+1]==1){
+                count++;
+                //i=i+1;
+            }
+            
+        }
+        if(count ==1) return true;
+        else return false;
+    }
+};
